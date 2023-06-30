eb73356d227fa867d2464b060427dc5a38e07e9a
Henry Timms <36480544+Hen676@users.noreply.github.com>
2023-06-30 21:57:14 +0100

    Client messages for key binding toggles

--- src/main/java/hen676/dragonlite/keybinds/HealthBarKeybinding.java
+++ src/main/java/hen676/dragonlite/keybinds/HealthBarKeybinding.java
@@ -6,6 +6,9 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.Style;
+import net.minecraft.text.Text;
+import net.minecraft.util.DyeColor;
 import org.lwjgl.glfw.GLFW;
 
 @Environment(EnvType.CLIENT)
@@ -27,6 +30,14 @@ public class HealthBarKeybinding {
                 if (client.world == null)
                     return;
                 toggle = !toggle;
+                if (client.player != null) {
+                    Text text = Text.translatable("message.dragonlite.health_bar").append(": ");
+                    if (toggle)
+                        text = text.copy().append(Text.translatable("message.dragonlite.on").setStyle(Style.EMPTY.withColor(DyeColor.GREEN.getSignColor())));
+                    else
+                        text = text.copy().append(Text.translatable("message.dragonlite.off").setStyle(Style.EMPTY.withColor(DyeColor.RED.getSignColor())));
+                    client.player.sendMessage(text,true);
+                }
             }
         });
     }
